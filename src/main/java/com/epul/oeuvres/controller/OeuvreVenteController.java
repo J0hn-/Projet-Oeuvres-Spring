aaee7b812f90c9108e27e0d4c12646bed237ba45
J0hn <jonathan.platteau@yahoo.fr>
2016-04-23 13:18:20 +0200

    OeuvreVente controller

--- src/main/java/com/epul/oeuvres/controller/OeuvreVenteController.java
+++ src/main/java/com/epul/oeuvres/controller/OeuvreVenteController.java
@@ -1,137 +1,117 @@
 package com.epul.oeuvres.controller;
 
-import dao.ServiceOeuvre;
-import dao.ServiceProprietaire;
-import meserreurs.MonException;
-import metier.Oeuvrevente;
-import metier.Proprietaire;
+import com.epul.oeuvres.dao.ServiceOeuvre;
+import com.epul.oeuvres.dao.ServiceProprietaire;
+import com.epul.oeuvres.meserreurs.MonException;
+import com.epul.oeuvres.metier.Oeuvrevente;
+import com.epul.oeuvres.metier.Proprietaire;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
 
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
-/**
- * Servlet implementation class AdherentController
- */
-@WebServlet("/oeuvre")
-public class OeuvreVenteController extends HttpServlet {
-    private static final long serialVersionUID = 1L;
+@Controller
+public class OeuvreVenteController extends MultiActionController {
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public OeuvreVenteController() {
-        super();
-    }
+//	private static final Logger logger = LoggerFactory.getLogger(MultiControleur.class);
 
-    /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-     *      response)
-     */
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processusTraiteRequete(request, response);
-    }
+    @RequestMapping(value = "/oeuvre/lister")
+    public ModelAndView lister(HttpServletRequest request) throws Exception {
+        String destinationPage;
 
-    /**
-     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-     *      response)
-     */
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processusTraiteRequete(request, response);
-    }
-
-    protected void processusTraiteRequete(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        String actionName = request.getParameter("action");
-        String destinationPage = "/pages/erreur.jsp";
-
-        // execute l'action
-        switch(actionName)
-        {
-            case "lister":
-                try {
-                    request.setAttribute("mesOeuvres", ServiceOeuvre.gets());
-                } catch (MonException e) {
-                    e.printStackTrace();
-                }
-
-                destinationPage = "/pages/oeuvre/liste.jsp";
-                break;
-            case "ajout" :
-                try {
-                    request.setAttribute("proprios", ServiceProprietaire.gets());
-                } catch (MonException e) {
-                    e.printStackTrace();
-                }
-                destinationPage = "/pages/oeuvre/ajout.jsp";
-                break;
-            case "modif" :
-                try {
-                    request.setAttribute("proprios", ServiceProprietaire.gets());
-                    request.setAttribute("o", ServiceOeuvre.get(Integer.parseInt(request.getParameter("oeuvre"))));
-                } catch (MonException e) {
-                    e.printStackTrace();
-                }
-
-                destinationPage = "/pages/oeuvre/modif.jsp";
-                break;
-            case "modifier" :
-                try {
-                    Oeuvrevente ov = ServiceOeuvre.get(Integer.parseInt(request.getParameter("id")));
-                    ov.setTitreOeuvrevente(request.getParameter("titre"));
-                    ov.setEtatOeuvrevente(request.getParameter("etat"));
-                    ov.setPrixOeuvrevente(Float.parseFloat(request.getParameter("prix")));
-                    ov.setProprietaire(ServiceProprietaire.get(Integer.parseInt(request.getParameter("proprio"))));
-                    ServiceOeuvre.modif(ov);
-
-                } catch (MonException e) {
-                    e.printStackTrace();
-                }
-                destinationPage = "/index.jsp";
-                break;
-            case "proprio" :
-                destinationPage = "/pages/oeuvre/proprio.jsp";
-                break;
-            case "insertion" :
-                try {
-                    Proprietaire p = new Proprietaire();
-                    p.setNomProprietaire(request.getParameter("nom"));
-                    p.setPrenomProprietaire(request.getParameter("prenom"));
-                    ServiceProprietaire.insert(p);
-
-                } catch (MonException e) {
-                    e.printStackTrace();
-                }
-                destinationPage = "/index.jsp";
-                break;
-            case "inserer" :
-                try {
-                    Oeuvrevente ov = new Oeuvrevente();
-                    ov.setTitreOeuvrevente(request.getParameter("titre"));
-                    ov.setEtatOeuvrevente(request.getParameter("etat"));
-                    ov.setPrixOeuvrevente(Float.parseFloat(request.getParameter("prix")));
-                    ov.setProprietaire(ServiceProprietaire.get(Integer.parseInt(request.getParameter("proprio"))));
-                    ServiceOeuvre.insert(ov);
-
-                } catch (MonException e) {
-                    e.printStackTrace();
-                }
-                destinationPage = "/index.jsp";
-                break;
-            default:
-                String messageErreur = "[" + actionName + "] n'est pas une action valide.";
-                request.setAttribute("messageErreur", messageErreur);
+        try {
+            request.setAttribute("mesOeuvres", ServiceOeuvre.gets());
+            destinationPage = "/pages/oeuvre/liste";
+        } catch (MonException e) {
+            request.setAttribute("MesErreurs", e.getMessage());
+            destinationPage = "Erreur";
         }
 
-        // Redirection vers la page jsp appropriee
-        RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(destinationPage);
-        dispatcher.forward(request, response);
+        return new ModelAndView(destinationPage);
+    }
+
+    @RequestMapping(value = "/oeuvre/inserer")
+    public ModelAndView inserer(HttpServletRequest request) throws Exception {
+
+        String destinationPage;
+        try {
+            Oeuvrevente ov = new Oeuvrevente();
+            ov.setTitreOeuvrevente(request.getParameter("titre"));
+            ov.setEtatOeuvrevente(request.getParameter("etat"));
+            ov.setPrixOeuvrevente(Float.parseFloat(request.getParameter("prix")));
+            ov.setProprietaire(ServiceProprietaire.get(Integer.parseInt(request.getParameter("proprio"))));
+            ServiceOeuvre.insert(ov);
+            destinationPage = "home";
+        } catch (Exception e) {
+            request.setAttribute("MesErreurs", e.getMessage());
+            destinationPage = "Erreur";
+        }
+
+        return new ModelAndView(destinationPage);
+    }
+
+    @RequestMapping(value = "/oeuvre/ajout")
+    public ModelAndView ajouter(HttpServletRequest request) throws Exception {
+        request.setAttribute("proprios", ServiceProprietaire.gets());
+        return new ModelAndView("/pages/oeuvre/ajout");
+    }
+
+    @RequestMapping(value = "/oeuvre/proprio")
+    public ModelAndView proprio() throws Exception {
+        return new ModelAndView("/pages/oeuvre/proprio");
+    }
+
+    @RequestMapping(value = "/oeuvre/modif")
+    public ModelAndView modifier(HttpServletRequest request) throws Exception {
+        String destinationPage;
+        try {
+            request.setAttribute("proprios", ServiceProprietaire.gets());
+            request.setAttribute("o", ServiceOeuvre.get(Integer.parseInt(request.getParameter("oeuvre"))));
+            destinationPage = "/pages/oeuvre/modif";
+        } catch (MonException e) {
+            request.setAttribute("MesErreurs", e.getMessage());
+            destinationPage = "Erreur";
+        }
+
+        return new ModelAndView(destinationPage);
+    }
+
+    @RequestMapping(value = "/oeuvre/modifier")
+    public ModelAndView modifierOeuvre(HttpServletRequest request) throws Exception {
+        String destinationPage;
+        try {
+            Oeuvrevente ov = ServiceOeuvre.get(Integer.parseInt(request.getParameter("id")));
+            ov.setTitreOeuvrevente(request.getParameter("titre"));
+            ov.setEtatOeuvrevente(request.getParameter("etat"));
+            ov.setPrixOeuvrevente(Float.parseFloat(request.getParameter("prix")));
+            ov.setProprietaire(ServiceProprietaire.get(Integer.parseInt(request.getParameter("proprio"))));
+            ServiceOeuvre.modif(ov);
+            destinationPage = "home";
+        } catch (Exception e) {
+            request.setAttribute("MesErreurs", e.getMessage());
+            destinationPage = "Erreur";
+        }
+
+        return new ModelAndView(destinationPage);
+    }
+
+    @RequestMapping(value = "/oeuvre/insertion")
+    public ModelAndView insertionProprio(HttpServletRequest request) throws Exception {
+        String destinationPage;
+        try {
+            Proprietaire p = new Proprietaire();
+            p.setNomProprietaire(request.getParameter("nom"));
+            p.setPrenomProprietaire(request.getParameter("prenom"));
+            ServiceProprietaire.insert(p);
+            destinationPage = "home";
+        } catch (Exception e) {
+            request.setAttribute("MesErreurs", e.getMessage());
+            destinationPage = "Erreur";
+        }
+
+        return new ModelAndView(destinationPage);
     }
 
 }
